c237a8cd5d8290dbaaf4a673bfc2d5e1a40b8577
MathCoimbra <matheuscoimbra35@gmail.com>
2022-06-24 13:04:19 -0300

    adicionado projetos do curso em vídeo

---
+++ src/main/java/com/projeto/curso/em/video/screenresolution/ScreenResolution.java
@@ -0,0 +1,14 @@
+package com.projeto.curso.em.video.screenresolution;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class ScreenResolution {
+	
+    public static void main(String[] args) {
+        Toolkit tk;
+        tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.print("Your screen resolution is "+d.width + " X " +d.height);
+    }
+}
