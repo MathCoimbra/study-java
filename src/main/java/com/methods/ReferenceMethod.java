a20f80554f7dddab38734bea5bb7d1ff7490cc83
MathCoimbra <matheuscoimbra35@gmail.com>
2022-09-27 12:02:07 -0300

    add reference method example

---
+++ src/main/java/com/methods/ReferenceMethod.java
@@ -0,0 +1,66 @@
+package com.methods;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+public class ReferenceMethod {
+
+    public static void main(String[] args) {
+
+        List<Car> myCars = new ArrayList<>() {{
+            add(new Car("Gol", 1253, "azul"));
+            add(new Car("Pálio", 5512, "branco"));
+            add(new Car("Lancer", 5312, "preto"));
+        }};
+
+        // Sem reference method
+
+        myCars.sort(Comparator.comparing((Car car) -> car.getName()));
+
+        // Com reference method
+
+        myCars.sort(Comparator.comparing(Car::getName));
+
+    }
+
+}
+
+class Car {
+
+    private String name;
+    private Integer placa;
+    private String cor;
+
+    public Car(String name, Integer placa, String cor) {
+        super();
+        this.name = name;
+        this.placa = placa;
+        this.cor = cor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(Integer placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
